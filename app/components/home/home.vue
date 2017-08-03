0f7497b14334b69b62c513525c1adef81b96db4d
Dan Osborne <dan.osborne@rocketmail.com>
2017-08-03 14:21:23 +0100

    Added word logic to homepage

--- app/components/home/home.vue
+++ app/components/home/home.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>
-        <h1>This is a test</h1>
+    <div class="home">
+        <div class="centre">
+            <h1>{{currentWord}}</h1>
+            <h2>Wave</h2>
+        </div>
     </div>
 </template>
+
+<script>
+    const wordIndex = (location.search) ? location.search.replace('?word=', '') : 0;
+
+    import words from '../../shared/words-clean.json';
+    export default {
+        computed: {
+            currentWord() {
+                return words[wordIndex];
+            }
+        }
+    }
+</script>
