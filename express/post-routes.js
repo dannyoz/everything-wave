b0719cb494d41ff9eff4ae4cd849e7802d27ff1e
Dan Osborne <dan.osborne@rocketmail.com>
2017-08-09 15:17:46 +0100

    tweet route added

--- express/post-routes.js
+++ express/post-routes.js
@@ -2,11 +2,21 @@ const routes = {};
 const constants = require('../app/shared/constants.js');
 const HTTP = require('superagent');
 const webshot = require('webshot');
+const twit = require('twit');
 const fs = require('fs');
+const del = require('node-delete');
 const words = require('../app/shared/words-clean.json');
 const screenshot = require('screenshot-stream');
 const environment = process.env.NODE_ENV || 'development';
 const envPath = './environments/'+environment+'/';
+
+const Twit = new twit({
+    consumer_key:         process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+    access_token:         process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    timeout_ms:           60*1000,  
+});
 
 routes[`${constants.apiVersion}wordlist`] = function(req, res){
 
@@ -75,7 +85,7 @@ routes[`${constants.apiVersion}nextword`] = function(req, res) {
         }
 
     });
-}
+};
 
 routes[`${constants.apiVersion}clean-json`] = function(req, res) {
     var headerSent = false;
@@ -94,6 +104,110 @@ routes[`${constants.apiVersion}clean-json`] = function(req, res) {
             res.status(200).send({data: 'Success'});
         }
     });
+};
+
+routes[`${constants.apiVersion}clean`] = function(req, res) {
+    var file = envPath + '/img/test.png';
+    del([file], function (err, paths) {
+        console.log('Deleted files/folders:\n', paths.join('\n'));
+        if(err) {
+            res.status(500).send(err);
+        } else  {
+            res.status(200).send({data: 'file deleted'});
+        }
+    });
+};
+
+routes[`${constants.apiVersion}tweet`] = function(req, res) {
+
+    nextWord();
+
+    function nextWord() {
+        console.log('Generating new word...');
+
+        var headerSent = false;
+        var dest = 'app/shared/current-word.json';
+        var newJson = createNext();
+        var word = "";
+        
+        function createNext() {
+            var content = fs.readFileSync(dest, 'utf8');
+            var current = JSON.parse(content);
+            var nextIndex = current.index + 1;
+            var nextWord = words[nextIndex];
+            var newString = JSON.stringify({
+                index: nextIndex,
+                text: nextWord
+            });
+            word = current.text;
+            return newString;
+        };
+
+        fs.writeFile(dest, newJson, (err) => {
+            if(err) {
+                res.status(500).send(err);
+            } else if(!headerSent) {
+                headerSent = true;
+                newJson = createNext();
+                console.log('New word generated!');
+                screenGrab(word);
+            }
+        });
+    }
+
+    function screenGrab(word) {
+        console.log('Generating screen grab...');
+        var url = `http://localhost:5000/word/`;
+        var file = envPath + '/img/test.png';
+
+        webshot(url, file, function(err) {
+            if(err) {
+                res.status(500).send(err);
+            } else {
+                console.log('Screen grab generated!');
+                tweet(word);
+            }
+        });
+    };
+    
+    function tweet(word) {
+        console.log('Creating tweet...');
+        var file = envPath + '/img/test.png';
+        var b64content = fs.readFileSync(file, { encoding: 'base64' });
+
+        Twit.post('media/upload', { media_data: b64content }, function (err, data, response) {
+            // now we can assign alt text to the media, for use by screen readers and 
+            // other text-based presentations and interpreters 
+            var mediaIdStr = data.media_id_string
+            var altText = word + 'wave';
+            var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+            
+            Twit.post('media/metadata/create', meta_params, function (err, data, response) {
+                if (!err) {
+                    // now we can reference the media and post a tweet (media will attach to the tweet) 
+                    var params = { status: word + 'wave', media_ids: [mediaIdStr] }
+                
+                    Twit.post('statuses/update', params, function (err, data, response) {
+                        console.log('Tweet created!');
+                        clean();
+                    })
+                }
+            });
+        });
+    };
+
+    function clean() {
+        console.log('Cleaning directory...');
+        var file = envPath + '/img/test.png';
+        del([file], function (err, paths) {
+            if(err) {
+                res.status(500).send(err);
+            } else {
+                console.log('Deleted files/folders:\n', paths.join('\n'));
+                res.status(200).send({data: 'success'});
+            }
+        });
+    };
 };
 
 module.exports = routes;
